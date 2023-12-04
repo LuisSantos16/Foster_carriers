1279579d485dd5123e51ba2a69d8d2938dc0feab
LuisSantos16 <lmsr.1601@gmail.com>
2023-12-04 15:56:03 -0500

    quote section update 2

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,17 @@
-//menu
+// Elimina el fragmento de la URL sin recargar la página
+function handlePopState() {
+    history.replaceState(null, null, window.location.pathname);
+}
+    window.addEventListener('popstate', handlePopState);
+    document.addEventListener('DOMContentLoaded', handlePopState);
 
-function llamar(){
+//menu
+function llamar() {
     let menudespegable = document.getElementById("listaid");
-    menudespegable.classList.toggle("lista-open")
-    let listabar=document.getElementById("lista-bar");
-    listabar.querySelector("lista-bar .listabar").classList.toggle;
+    menudespegable.classList.toggle("lista-open");
+    
+    let listabar = document.getElementById("lista-barid");
+    listabar.querySelector(".lista-bar .listabar").classList.toggle("clase-a-togglear");
 }
 
 //Boton Subir
@@ -20,4 +27,4 @@ var npos=window.pageYOffset;
             subir.style.display="none";
             subir.style.transition="0.5s";
         }
-    }
+}
